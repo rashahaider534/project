73e9b4d1679f9cac90fdf787535127966729a565
rasha <rsha74877@gmail.com>
2024-12-23 17:13:41 -0800

    change authrequset

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->integer('phone')->unique();
+            $table->string('phone')->unique();
             $table->string('password');
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
